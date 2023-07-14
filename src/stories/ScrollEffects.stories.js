6eadbb42d52f80b9a46bb26990007ddfc4f1d08e
Fidel Ernesto <fidehlg89@gmail.com>
2023-07-13 21:26:52 -0400

    feat: add ScrollEffects Example

---
+++ src/stories/ScrollEffects.stories.js
@@ -0,0 +1,14 @@
+import ScrollEffects from "../components/ScrollEfffects/ScrollEffects";
+
+export default {
+  title: "Components/ScrollEffects",
+  component: ScrollEffects,
+};
+
+const Template = (args) => <ScrollEffects {...args} />
+
+export const Basic = Template.bind({});
+
+Basic.args = {
+
+};
