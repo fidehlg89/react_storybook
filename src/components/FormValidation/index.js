931aa4d5f41c3a4871bec993534d0e5984e0c1ed
Fidel Ernesto <fidehlg89@gmail.com>
2023-10-30 23:49:32 -0400

    feat: Form validation added more elements

--- src/components/FormValidation/index.js
+++ src/components/FormValidation/index.js
@@ -2,7 +2,7 @@ import "./FormValidation.css";
 
 const FormValidation = (props) => {
   return (
-    <form onSubmit={e=>e.preventDefault()}>
+    <form onSubmit={(e) => e.preventDefault()}>
       <div>
         <label for="choose">Would you prefer a banana or a cherry?</label>
         <input
@@ -25,6 +25,33 @@ const FormValidation = (props) => {
           max="10"
         />
       </div>
+      <label for="t1">
+        What's your favorite fruit?<span aria-label="required">*</span>
+      </label>
+      <input
+        type="text"
+        id="t1"
+        name="fruit"
+        list="l1"
+        required
+        pattern="[Bb]anana|[Cc]herry|[Aa]pple|[Ss]trawberry|[Ll]emon|[Oo]range"
+      />
+      <datalist id="l1">
+        <option>Banana</option>
+        <option>Cherry</option>
+        <option>Apple</option>
+        <option>Strawberry</option>
+        <option>Lemon</option>
+        <option>Orange</option>
+      </datalist>
+      <p>
+        <label for="t2">What's your email address?</label>
+        <input type="email" id="t2" name="email" />
+      </p>
+      <p>
+        <label for="t3">Leave a short message</label>
+        <textarea id="t3" name="msg" maxlength="140" rows="5"></textarea>
+      </p>
       <div>
         <button>Submit</button>
       </div>
