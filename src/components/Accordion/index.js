6c28aba5f79d9561ce03e367c82952797b4eb4e8
Fidel Ernesto <fidehlg89@gmail.com>
2024-01-15 10:34:39 -0500

    feat: add accordion

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,24 +1,50 @@
-import React from 'react'
+import styled from 'styled-components';
+
+const AccordionSection = styled.section`
+  width: 80%;
+  margin: 0 auto;
+  font-family: Arial, sans-serif;
+`;
+
+const AccordionDetails = styled.details`
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-bottom: 10px;
+`;
+
+const AccordionSummary = styled.summary`
+  background-color: #f0f0f0;
+  padding: 10px;
+  cursor: pointer;
+`;
+
+const AccordionContent = styled.div`
+  padding: 10px;
+`;
 
 const Accordion = () => {
   return (
-    <section>
-        <details name="info">
-          <summary>Hola Mundo</summary>
+    <AccordionSection>
+      <AccordionDetails name="info">
+        <AccordionSummary>Hola Mundo</AccordionSummary>
+        <AccordionContent>
           <p>Este es el texto de Hola Mundo content</p>
-        </details>
-        <details name="info">
-          <summary>
-            Accordion Title
-          </summary>
+        </AccordionContent>
+      </AccordionDetails>
+      <AccordionDetails name="info">
+        <AccordionSummary>Accordion Title</AccordionSummary>
+        <AccordionContent>
           <p>Este es el título del artículo</p>
-        </details>
-        <details name="info">
-        <summary>Otra Sección</summary>
+        </AccordionContent>
+      </AccordionDetails>
+      <AccordionDetails name="info">
+        <AccordionSummary>Otra Sección</AccordionSummary>
+        <AccordionContent>
           <p>Este es otro item de carousel</p>
-        </details>
-    </section>
-  )
-}
+        </AccordionContent>
+      </AccordionDetails>
+    </AccordionSection>
+  );
+};
 
-export default Accordion
+export default Accordion;
